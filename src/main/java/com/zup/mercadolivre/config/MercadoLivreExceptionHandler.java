1686c90ef8e9ab7d71cad48b82e8c7ff0d609eae
Matheus <matheus.santos2@zup.com>
2020-10-07 13:00:14 -0300

    Added detailed exception handler

---
+++ src/main/java/com/zup/mercadolivre/config/MercadoLivreExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.zup.mercadolivre.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MercadoLivreExceptionHandler {
+    
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ExceptionDto argumentNotValidException(MethodArgumentNotValidException e) {
+        return new ExceptionDto(e.getBindingResult().getFieldError().getField().toString(), 
+                    HttpStatus.BAD_REQUEST.toString(), e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
+}
