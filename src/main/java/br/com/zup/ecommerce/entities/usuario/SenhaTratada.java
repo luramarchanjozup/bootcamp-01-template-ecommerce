907a0287a28e7e18a7534165c6fa55ea42805c17
Fernanda Vedovello <fe.vedovello@gmail.com>
2020-10-14 18:38:08 -0300

    Utilizar BCryptPasswordEncoder

--- src/main/java/br/com/zup/ecommerce/entities/usuario/SenhaTratada.java
+++ src/main/java/br/com/zup/ecommerce/entities/usuario/SenhaTratada.java
@@ -1,16 +1,13 @@
 package br.com.zup.ecommerce.entities.usuario;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.util.Assert;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
- * Contagem de carga intrínseca da classe: 2
+ * Contagem de carga intrínseca da classe: 0
  */
 
 public class SenhaTratada {
@@ -22,31 +19,11 @@ public class SenhaTratada {
         Assert.notNull(senha, "A senha não pode ser vazia");
         Assert.isTrue(senha.length() >= 6, "A senha deve ter tamanho igual ou maior que 6");
 
-        this.senha = this.criptografarSenha(senha);
+        this.senha = new BCryptPasswordEncoder().encode(senha);
     }
 
     public String getSenha() {
         return senha;
     }
 
-    private String criptografarSenha(String senha) {
-        MessageDigest algotimo;
-        byte[] sDigest = new byte[0];
-
-        //2
-        try {
-            algotimo = MessageDigest.getInstance("SHA-256");
-            sDigest = algotimo.digest(senha.getBytes(StandardCharsets.UTF_8));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        StringBuilder hexStringSenha = new StringBuilder();
-        //1
-        for (byte b : sDigest) {
-            hexStringSenha.append(String.format("%02X", 0xFF & b));
-        }
-
-        return hexStringSenha.toString();
-    }
 }
