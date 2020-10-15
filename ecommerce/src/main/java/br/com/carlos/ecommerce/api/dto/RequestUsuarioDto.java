30c07aa76e66f4297374ba6d7ada5bfa81f5b6b2
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-15 19:30:38 -0300

    Dtos de categorias e usuarios

---
+++ ecommerce/src/main/java/br/com/carlos/ecommerce/api/dto/RequestUsuarioDto.java
@@ -0,0 +1,24 @@
+package br.com.carlos.ecommerce.api.dto;
+
+import br.com.carlos.ecommerce.api.handler.Unique;
+import br.com.carlos.ecommerce.domain.entity.Usuario;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class RequestUsuarioDto {
+
+    @NotBlank @Email @Unique(domainClass = Usuario.class, fieldName = "login")
+    private String login;
+    @NotBlank @Length(min = 6, message = "Tamanho mínimo de 6 carateres")
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
