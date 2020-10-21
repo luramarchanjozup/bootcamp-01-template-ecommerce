7587263653edbcbdebdda08efc8ee4e266998590
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-21 10:47:31 -0300

    adicionando anotações importantes

--- ecommerce/src/main/java/br/com/carlos/ecommerce/api/handler/Unique.java
+++ ecommerce/src/main/java/br/com/carlos/ecommerce/api/handler/Unique.java
@@ -1,7 +1,16 @@
 package br.com.carlos.ecommerce.api.handler;
 
+import javax.validation.Constraint;
 import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Constraint(validatedBy = {UniqueValidator.class})
+@Target({FIELD})
+@Retention(RUNTIME)
 public @interface Unique {
     String message() default "duplicado";
     Class<? extends Payload>[] payload() default { };
