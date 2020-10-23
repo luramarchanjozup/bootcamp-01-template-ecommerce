2d403b5bc2534f9cce6615f55fb2f8b919662599
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-23 08:13:14 -0300

    adicionando mapeamento e usando LocalDateTime

--- ecommerce/src/main/java/br/com/carlos/ecommerce/domain/entity/Produto.java
+++ ecommerce/src/main/java/br/com/carlos/ecommerce/domain/entity/Produto.java
@@ -1,6 +1,7 @@
 package br.com.carlos.ecommerce.domain.entity;
 
 import br.com.carlos.ecommerce.api.dto.RequestCaracteristicaDto;
+import br.com.carlos.ecommerce.domain.service.OpinioesService;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.util.Assert;
@@ -11,10 +12,9 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Entity
@@ -49,8 +49,17 @@ public class Produto {
     @OneToMany(mappedBy = "produto", cascade = CascadeType.MERGE)
     private Set<ImagemProduto> imagens = new HashSet<>();
 
+    @OneToMany(mappedBy = "produto")
+    @OrderBy("titulo asc")
+    private SortedSet<Pergunta> perguntas = new TreeSet<>();
+
+
+
+    @OneToMany(mappedBy = "produto", cascade = CascadeType.MERGE)
+    private Set<Opiniao> opinioes  = new HashSet<>();
+
     @CreationTimestamp
-    private Date timestamp;
+    private LocalDateTime timestamp;
 
 
     @Deprecated
@@ -72,26 +81,10 @@ public class Produto {
         this.comprador = comprador;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        return result;
-    }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Produto other = (Produto) obj;
-        if (nome == null) {
-            return other.nome == null;
-        } else return nome.equals(other.nome);
+
+    public SortedSet<Pergunta> getPerguntas() {
+        return perguntas;
     }
 
     public void associaImagens(Set<String> links) {
@@ -125,4 +118,65 @@ public class Produto {
     public BigDecimal getValor() {
         return valor;
     }
+
+    public Set<ImagemProduto> getImagens() {
+        return imagens;
+    }
+
+    public OpinioesService getOpinioes() {
+        return new OpinioesService(this.opinioes);
+    }
+
+    public <T> Set<T> mapeiaCaracteristicas(
+            Function<CaracteristicaProduto, T> funcaoMapeadora) {
+        return this.caracteristicas.stream().map(funcaoMapeadora)
+                .collect(Collectors.toSet());
+    }
+
+    public <T> Set<T> mapeiaImagens(Function<ImagemProduto, T> funcaoMapeadora) {
+        return this.imagens.stream().map(funcaoMapeadora)
+                .collect(Collectors.toSet());
+    }
+
+    public <T extends Comparable<T>> SortedSet<T> mapeiaPerguntas(Function<Pergunta, T> funcaoMapeadora) {
+        return this.perguntas.stream().map(funcaoMapeadora)
+                .collect(Collectors.toCollection(TreeSet :: new));
+    }
+
+
+    public boolean abataEstoque(@Positive int quantidade) {
+        Assert.isTrue(quantidade > 0, "A quantidade deve ser maior que zero para abater o estoque "+quantidade);
+
+        if(quantidade <= this.quantidade) {
+            this.quantidade-=quantidade;
+            return true;
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (nome == null) {
+            return other.nome == null;
+        } else return nome.equals(other.nome);
+    }
+
+
 }
